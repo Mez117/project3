a1e23804bb66274007531b6151c74a3b5d69f6e1
James <jamesholton2511@gmail.com>
2023-06-16 05:08:34 +1000

    fix frames?

--- 07/game-07.js
+++ 07/game-07.js
@@ -379,7 +379,7 @@ Game.Player.prototype = {
     if (!this.jumping && this.velocity_y < 10) {
 
       this.jumping     = true;
-      this.velocity_y -= 13;
+      this.velocity_y -= 25;
 
     }
 
@@ -454,14 +454,14 @@ Game.TileSet = function(columns, tile_size) {
 
   let f = Game.Frame;
 
-  this.frames = [new f(270,  0, 19, 29, 0, -4), // idle-left
-                 new f(230,  0, 19, 28, 0, -4), // jump-left
-                 new f(136,  0, 24, 25, 0, -4), new f(188, 0, 24, 27, 0, -4), new f(32, 0, 28, 28, 0, -4), new f(136,  0, 24, 25, 0, -4), // walk-left
-                 new f(289,  0, 19, 29, 0, -4), // idle-right
-                 new f(249,  0, 19, 28, 0, -4), // jump-right
-                 new f(160, 0, 24, 25, 0, -4), new f(212, 0, 24, 27, 0, -4), new f(60, 0, 28, 28, 0, -4), new f(160, 0, 24, 25, 0, -4), // walk-right
+  this.frames = [new f(270,  0, 19, 29, 0, -10), // idle-left
+                 new f(230,  0, 19, 28, 0, -10), // jump-left
+                 new f(136,  0, 24, 25, 0, -10), new f(188, 0, 24, 27, 0, -10), new f(32, 0, 28, 28, 0, -10), new f(136,  0, 24, 25, 0, -10), // walk-left
+                 new f(289,  0, 19, 29, 0, -10), // idle-right
+                 new f(249,  0, 19, 28, 0, -10), // jump-right
+                 new f(160, 0, 24, 25, 0, -10), new f(212, 0, 24, 27, 0, -10), new f(60, 0, 28, 28, 0, -10), new f(160, 0, 24, 25, 0, -10), // walk-right
                  new f(160, 32, 32, 32), new f(192, 32, 32, 32), // carrot
-                 new f(0, 0, 32, 12), new f(0, 12, 32, 10), new f(0, 24, 32, 10) // grass
+                 new f(0, 0, 32, 12, 0, 7), new f(0, 12, 32, 10, 0, 10), new f(0, 24, 32, 10, 0, 9) // grass
                 ];
 
 };
